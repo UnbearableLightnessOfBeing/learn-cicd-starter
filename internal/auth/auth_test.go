2a4e5414ef88b8d6c9875e1c240da43244d193b9
Lada Crin <golomazdinp@gmail.com>
2024-10-15 02:16:57 +0300

    add formatting to ci

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -8,39 +8,39 @@ import (
 )
 
 func TestFailsIfAuthHeaderNotIncluded(t *testing.T) {
-  header := make(map[string][]string)
+	header := make(map[string][]string)
 
-  key, err := auth.GetAPIKey(header)
-  if key != "" {
-    t.Fatal("key should be empty")
-  }
-  if !errors.Is(err, auth.ErrNoAuthHeaderIncluded) {
-    t.Fatal("should be header not included error")
-  }
+	key, err := auth.GetAPIKey(header)
+	if key != "" {
+		t.Fatal("key should be empty")
+	}
+	if !errors.Is(err, auth.ErrNoAuthHeaderIncluded) {
+		t.Fatal("should be header not included error")
+	}
 }
 
 func TestFailsOnMalformedAuthHeader(t *testing.T) {
-  header := make(map[string][]string)
-  header["Authorization"] = []string{"ApiKey_aboba"}
+	header := make(map[string][]string)
+	header["Authorization"] = []string{"ApiKey_aboba"}
 
-  key, err := auth.GetAPIKey(header)
-  if key != "" {
-    t.Fatal("key should be empty")
-  }
-  if err == nil {
-    t.Fatal("should error")
-  }
+	key, err := auth.GetAPIKey(header)
+	if key != "" {
+		t.Fatal("key should be empty")
+	}
+	if err == nil {
+		t.Fatal("should error")
+	}
 }
 
 func TestGetApiKeySuccessfully(t *testing.T) {
-  header := make(map[string][]string)
-  header["Authorization"] = []string{"ApiKey aboba"}
+	header := make(map[string][]string)
+	header["Authorization"] = []string{"ApiKey aboba"}
 
-  key, err := auth.GetAPIKey(header)
-  if err != nil {
-    t.Fatal("shouldn't error")
-  }
-  if key != "aboba" {
-    t.Fatal("go wrong key")
-  }
+	key, err := auth.GetAPIKey(header)
+	if err != nil {
+		t.Fatal("shouldn't error")
+	}
+	if key != "aboba" {
+		t.Fatal("go wrong key")
+	}
 }
